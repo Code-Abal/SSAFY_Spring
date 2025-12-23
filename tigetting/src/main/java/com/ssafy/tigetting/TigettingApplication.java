9b5ce4d0a82148e2b0e13ef34ffaaeddbc4d6638
안도형 <dksehgud1@naver.com>
2025-12-23 09:36:45 +0900

    mapper error 수정

--- tigetting/src/main/java/com/ssafy/tigetting/TigettingApplication.java
+++ tigetting/src/main/java/com/ssafy/tigetting/TigettingApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.ssafy.tigetting.mapper")
+@MapperScan("com.ssafy.tigetting")
 public class TigettingApplication {
 
 	public static void main(String[] args) {
